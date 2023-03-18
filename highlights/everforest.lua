c9ccea8d8f56163e603ce308c63696b3be2e316a
Koslak <betancourtluismiguel@gmail.com>
2023-03-17 23:02:14 -0500

    Chaning the colors of the errors and warnings.

---
+++ highlights/everforest.lua
@@ -0,0 +1,7 @@
+return {
+  --Comment  = { fg = "#ffd580" },
+  VirtualTextError = { fg = "#ff6666" },
+  VirtualTextWarning = { fg = "#ffcc99" },
+  VirtualTextInfo = { fg = "#ffffff", bg = "#0088cc" },
+  VirtualTextHint = { fg = "#ffffff", bg = "#0088cc" },
+}
